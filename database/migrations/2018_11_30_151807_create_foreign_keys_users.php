cb91a34e8eab945773513c7ec0813726a9653ae7
B.Poignant <benjipoi@hotmail.com>
2018-11-30 16:25:10 +0100

    - création foreign key sur imports et queries - maj vue imports.index

---
+++ database/migrations/2018_11_30_151807_create_foreign_keys_users.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateForeignKeysUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+	    Schema::table('imports', function (Blueprint $table) {
+		    $table->unsignedInteger('user_id');
+		
+		    $table->foreign('user_id')->references('id')->on('users');
+	    });
+	
+	    Schema::table('queries', function (Blueprint $table) {
+		    $table->unsignedInteger('user_id');
+		
+		    $table->foreign('user_id')->references('id')->on('users');
+
+		    $table->unsignedInteger('import_id');
+		
+		    $table->foreign('import_id')->references('id')->on('imports');
+	    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+	    Schema::table('imports', function (Blueprint $table) {
+		    $table->dropForeign(['user_id']);
+	    });
+	
+	    Schema::table('queries', function (Blueprint $table) {
+		    $table->dropForeign(['user_id','import_id']);
+	    });
+    }
+}
--- resources/views/imports/index.blade.php
+++ resources/views/imports/index.blade.php
@@ -8,17 +8,35 @@
                 <div class="card-header">Imports index</div>
 
                 <div class="card-body">
-@if (session('status'))
-    <div class="alert alert-success">
-        {{ session('status') }}
-    </div>
-@endif
+					@if (session('status'))
+					<div class="alert alert-success">
+						{{ session('status') }}
+					</div>
+					@endif
 
-                    @foreach ($imports as $import)
-	   {{ $import->sync }} {{ $import->path }}<br />
-	@endforeach
-	
-	{{ $imports->links() }}
+                    @if ($imports)
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th> {{ __('Status') }}</th>
+                                    <th> {{ __('Path') }}</th>
+                                    <th> {{ __('Size') }}</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($imports as $import)
+                                @php
+echo '<pre>';var_dump( $import    );  echo '</pre>';    @endphp                    <tr>
+                                    <td>{{ $import->sync }}</td>
+                                    <td>{{ $import->path }}</td>
+                                    <td>{{ $import->size }}</td>
+                                </tr>
+                            @endforeach
+
+                            {{ $imports->links() }}
+                            </tbody>
+                        </table>
+                    @endif
                 </div>
             </div>
         </div>
