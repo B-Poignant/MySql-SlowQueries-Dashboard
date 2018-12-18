bb0777fcb88742dc4fce9cd01e9006c9e86c66fb
Benjamin <benjipoi@hotmail.com>
2018-12-18 13:59:37 +0100

    create test method in web

--- routes/web.php
+++ routes/web.php
@@ -26,15 +26,16 @@ Route::match('get','/queries/submit', 'QueriesController@submit')->name('queries
 Route::match('post','/queries/submit', 'QueriesController@post')->name('queries.post');
 
 Route::get('/imports/index', 'ImportsController@index')->name('imports.index');
-Route::match(['get','post'],'/imports/submit', 'ImportsController@submit')->name('imports.submit');
+Route::match('get','/imports/submit', 'ImportsController@submit')->name('imports.submit');
+Route::match('post','/imports/submit', 'ImportsController@post')->name('imports.post');
 
 Route::match('get', '/testSplitImport', function () {
-    $job = new \App\Jobs\SplitImport(18);
+    $job = new \App\Jobs\SplitImport(7);
     $job->handle();
 });
 
 Route::match('get', '/testStoreQueriesImport', function () {
-    $job = new \App\Jobs\StoreQueriesImport(18);
+    $job = new \App\Jobs\StoreQueriesImport(7);
     $job->handle();
 });
 
